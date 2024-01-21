5369f77e7fe4d783fd8c4ae42d5c1bf8f4219be6
Kenzo Yeo <Kenzo.Yeo@student.curtin.edu.sg>
2024-01-21 21:23:30 +0800

    Added unittest to covidvaccine.py

--- covidvaccine.py
+++ covidvaccine.py
@@ -1,3 +1,5 @@
+import unittest
+
 def El_COVID(age):
     result =""
     if age<0:
@@ -20,4 +22,57 @@ def El_COVID(age):
         result = "Invalid age"
 
     return result
+
+class TestElCOVID(unittest.TestCase):
+
+    def test_invalid_age_upper_boundary(self):
+        self.assertEqual(El_COVID(-1), "Invalid age")
+
+    def test_not_eligible_lower_boundary(self):
+        self.assertEqual(El_COVID(0), "Not eligible")
+        
+    def test_not_eligible_upper_boundary(self):
+        self.assertEqual(El_COVID(5), "Not eligible")
+    
+    def test_eligible_for_1_dose_lower_boundary(self):
+        self.assertEqual(El_COVID(6), "Eligible for 1 dose")
+
+    def test_eligible_for_1_dose_upper_boundary(self):
+        self.assertEqual(El_COVID(12), "Eligible for 1 dose")
+
+    def test_eligible_for_2_doses_lower_boundary(self):
+        self.assertEqual(El_COVID(13), "Eligible for 2 doses")
+        
+    def test_eligible_for_2_doses_upper_boundary(self):
+        self.assertEqual(El_COVID(16), "Eligible for 2 doses")
+        
+    def test_eligible_for_3_doses_lower_boundary(self):
+        self.assertEqual(El_COVID(17), "Eligible for 3 doses")
+
+    def test_eligible_for_3_doses_upper_boundary(self):
+        self.assertEqual(El_COVID(25), "Eligible for 3 doses")
+        
+    def test_eligible_for_optional_dose_lower_boundary(self):
+        self.assertEqual(El_COVID(26), "Eligible for optional dose")
+
+    def test_eligible_for_optional_dose_upper_boundary(self):
+        self.assertEqual(El_COVID(45), "Eligible for optional dose")
+
+    def test_must_take_optional_dose_lower_boundary(self):
+        self.assertEqual(El_COVID(46), "Must take optional dose")
+
+    def test_must_take_optional_dose_upper_boundary(self):
+        self.assertEqual(El_COVID(60), "Must take optional dose")
+
+    def test_see_your_gp_for_eligibility_lower_boundary(self):
+        self.assertEqual(El_COVID(61), "See your GP for eligibility")
+
+    def test_see_your_gp_for_eligibility_upper_boundary(self):
+        self.assertEqual(El_COVID(85), "See your GP for eligibility")
+
+    def test_invalid_age_lower_boundary(self):
+        self.assertEqual(El_COVID(86), "Invalid age")
+        
+if __name__ == '__main__':
+    unittest.main()
 

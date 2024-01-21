93c4c0a03c2a96d9827448201235a37f7f365cf6
Kenzo Yeo <Kenzo.Yeo@student.curtin.edu.sg>
2024-01-21 21:21:26 +0800

    Added unittest to mean.py

--- mean.py
+++ mean.py
@@ -1,3 +1,5 @@
+import unittest
+
 def funmean(num1, num2, num3, num4):
     '''
     A function takes four integer inputs (num1, num2, num3, num4), and returns the mean four numbers
@@ -8,3 +10,15 @@ def funmean(num1, num2, num3, num4):
     if num1>num2 and num3>=num4:
         mean_result = (num1+num2+num3+num4)/4
     return mean_result
+
+class TestFunMeanFunction(unittest.TestCase):
+
+    def test_valid_input(self):
+        self.assertEqual(funmean(5, 3, 8, 4), 5.0)
+
+    def test_invalid_input(self):
+        self.assertEqual(funmean(3, 5, 8, 4), -1)
+        self.assertEqual(funmean(5, 3, 4, 8), -1)
+
+if __name__ == '__main__':
+    unittest.main()

f108cf77f87343679e01b2400bfe69c597f3e411
Kenzo Yeo <Kenzo.Yeo@student.curtin.edu.sg>
2024-01-21 21:24:55 +0800

    Added unittest to median.py

--- median.py
+++ median.py
@@ -1,3 +1,6 @@
+import unittest
+from unittest.mock import patch
+
 def median():
     '''
     The funcation takes three values as input from user and calculates the median of the values. You are supposed to write a white box test for the code checking all paths in the code. Also, test the input and output on console (i.e. use test fixture concepts). 
@@ -13,5 +16,22 @@ def median():
     else: 
         median = value3
     return median
+
+class TestMedian(unittest.TestCase):
+
+    def test_if_path(self):
+        with patch('builtins.input', side_effect=[3, 1, 5]) as mock_input:
+            self.assertEqual(median(), 3)
+
+    def test_elif_path(self):
+        with patch('builtins.input', side_effect=[2, 4, 6]) as mock_input:
+            self.assertEqual(median(), 4)
+
+    def test_else_path(self):
+        with patch('builtins.input', side_effect=[2, 4, 3]) as mock_input:
+            self.assertEqual(median(), 3)
+
+if __name__ == '__main__':
+    unittest.main()
 
 

9ee4894b458586b25b07eac28b70787207b848bb
Kenzo Yeo <Kenzo.Yeo@student.curtin.edu.sg>
2024-01-21 21:25:43 +0800

    Improved overall modularity for studentnameid.py

--- studentnameid.py
+++ studentnameid.py
@@ -1,71 +1,37 @@
-'''
-The code in this software is designed to maintain the student records. 
-See the details of ach funcation
-'''
+def print_student_name(name):
+    print("Student Name:", name)
 
+def print_student_id(id_number):
+    print("Student ID:", id_number)
 
-def printNameID(value, isName):
-    #This funcation prints the value passed as an argument(parameter) based on
-    # on the value of IsName
-    if isName:
-        print("Student Name: ", value)
-    else:
-        print("Student ID: ",value)
+def get_value_size(value):
+    return len(value)
 
-
-def printvalue(value):
-    #This prints the value passed as an argument.
-    print("Value :", value)
-    
-
-
-def sizeofValue():
-    #This function calculates the size (i.e. number of characters) in 
-    #Stduent name and ID and store the size in global variables.
-    global name_size 
-    name_size = len(student_name)
-    global ID_size
-    ID_size = len(student_ID)
-
-
-
-def printNameChar():
-    #This function prints the stduent name charachter by character.
-    for ch in student_name:
+def print_name_characters(name):
+    for ch in name:
         print(ch, end=' ')
     print()
 
-def printIDChar():
-    #This funcation claculates the student ID charachter by character.
-    for num in student_ID:
+def print_id_characters(id_number):
+    for num in id_number:
         print(num, end=' ')
     print()
 
-
-def InputRecord():
-    #This function takes input from user and store the student name and ID
-    #in global variables. Call the other funcations for the printing and
-    #size calculationa and printing character by character.
-    global student_name
-    global student_ID
-
-    # taking input for student name and ID
+def main():
     student_name = input("Enter student name: ")
-    student_ID = input("Enter student ID: ")
+    student_id = input("Enter student ID: ")
 
-    
+    print_student_name(student_name)
+    print_student_id(student_id)
+
+    name_size = get_value_size(student_name)
+    id_size = get_value_size(student_id)
+
+    print("Name size:", name_size)
+    print("ID size:", id_size)
+
+    print_name_characters(student_name)
+    print_id_characters(student_id)
+
 if __name__ == "__main__":
-    
-    InputRecord()
-
-    sizeofValue()
-
-    printNameID(student_name, True)
-    printNameID(student_ID, False)
-
-    printvalue(name_size)
-    printvalue(ID_size)
-
-    printNameChar()
-    printIDChar()
-
+    main()
